3448f5b8c1134cffe339565d4652dc3d8f8c18f0
sman15tebo-web <sman15tebo.bos@gmail.com>
2026-06-17 13:01:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 
 // 1. Buat "Buku Alamat" untuk masing-masing sekolah
 const tenantConfig = {
-    "simisterbin": "https://script.google.com/macros/s/AKfycbwWorc4AgcOWI_451SlNTj7gmSBfQy8hqqfi2wJKK8R9lMrdS2dCgPnXFTi0P1fhLnXMQ/exec",
+    "demo": "https://script.google.com/macros/s/AKfycbwWorc4AgcOWI_451SlNTj7gmSBfQy8hqqfi2wJKK8R9lMrdS2dCgPnXFTi0P1fhLnXMQ/exec",
+    "sman15tebo": "https://script.google.com/macros/s/AKfycbwWorc4AgcOWI_451SlNTj7gmSBfQy8hqqfi2wJKK8R9lMrdS2dCgPnXFTi0P1fhLnXMQ/exec",
     "smk2": "https://script.google.com/macros/s/ID_API_SEKOLAH_3/exec"
     // Tambahkan sekolah lain di sini sesuai kebutuhan
 };
